d9e4e1be1a3090f3b4878a40b16c65468de80792
Andrew <adespinosa2002@gmail.com>
2026-04-28 22:10:42 -0700

    [FEAT] Added a popup showcasing all of the users

--- web_react/right_click/app/page.tsx
+++ web_react/right_click/app/page.tsx
@@ -1,45 +1,61 @@
 "use client"
 import { useState } from "react";
+import dynamic from "next/dynamic";
 import "./globals.css";
 import customers from "../data/customers.json";
+const CustomerMap = dynamic(() => import("./CustomerMap"), {ssr: false});
 
 export default function Home() {
+  return(
+    <>
 
-    return(
-      <>
-
-      <nav>
-        <div className="navbar-left">
-          <h1> <b> Right Click Assessment </b> </h1>
-        </div>
-      </nav>
-
-      <div className="main">
-        <h1> Welcome Back, Right Click! </h1>
-        <Parse_Customers />
+    <nav>
+      <div className="navbar-left">
+        <h1> <b> Right Click Assessment </b> </h1>
       </div>
+    </nav>
 
-      </>
-    );
+    <div className="main">
+      <h1> Welcome Back, Right Click! <ShowCustomers /></h1>
+      <br />
+      <CustomerMap />
+    </div>
+    </>
+  );
 }
 
-function Parse_Customers()
+function ShowCustomers()
 {
-  const [showList, setShowList] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   return(
     <>
-      <button type="button" className="btn" onClick={() => setShowList(!showList)}>
-        {showList ? "Hide Customers" : "Show Customers"}
-      </button>
-
-      {showList && (
-        <ul>
-          {customers.map((customer) => (
-            <li key={customer.id}> {customer.name} </li>
-          ))}
-        </ul>
+      <button onClick={() => setIsVisible(true)}>Show Customers</button>
+      {isVisible && (
+        <div style={{ position: "fixed", inset: 0, background: "rgba(0,0,0,0.5)", display: "flex", alignItems: "center", justifyContent: "center", zIndex: 1000 }}>
+          <div style={{ background: "white", padding: "2rem", borderRadius: "8px", maxHeight: "80vh", overflowY: "auto", minWidth: "300px" }}>
+            <button className="red_text" onClick={() => setIsVisible(false)}>Close</button>
+            <br />
+            <br />
+            <CustomerList />
+          </div>
+        </div>
       )}
+    </>
+  );
+}
+
+function CustomerList()
+{
+  return(
+    <>
+      {customers.map((customer) =>(
+        <>
+          <button type="button" key={customer.id} className="flex justify-center btn buttons_center"> {customer.name} </button>
+          <br />
+          <br />
+        </>
+      ))}
     </>
   );
 }
